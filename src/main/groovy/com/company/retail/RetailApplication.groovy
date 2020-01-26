a824377558f74a82140ff96ffeb24356295a519a
Eric <easycom89@gmail.com>
2020-01-26 15:39:47 -0600

    initial Commit

---
+++ src/main/groovy/com/company/retail/RetailApplication.groovy
@@ -0,0 +1,13 @@
+package com.company.retail
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class RetailApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(RetailApplication, args)
+	}
+
+}
